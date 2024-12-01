0d60ab0ea1416cee086645b84127873ebb243c8a
LucsRafa <lucasrafael66878@gmail.com>
2024-12-01 14:35:43 -0300

    Final2

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Configurações das variáveis de ambiente
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+# Certifique-se de que o ambiente virtual está ativado antes de rodar este script
+
+# Configuração do WSGI
 application = get_wsgi_application()
